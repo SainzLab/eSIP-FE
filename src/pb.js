da973ae3544ccb2a0d354ec82350de3f70e66c09
FazliXZ9 <fazlia322sok@gmail.com>
2026-04-20 10:29:38 +0700

    updt

--- src/pb.js
+++ src/pb.js
@@ -2,6 +2,8 @@ import PocketBase from 'pocketbase'
 
 const pbUrl = import.meta.env.VITE_PB_URL || 'http://192.168.8.10:8083'
 const pb = new PocketBase(pbUrl)
+// const url = 'http://163.61.58.220:8090'
+// const pb = new PocketBase(url)
 
 pb.autoCancellation(false)
 
